5be301c6071f39765ecc5c84f0bd0ed663d1ae0a
blue-pitaya <blue.pitaya@pm.me>
2022-09-28 21:46:29 +0200

    nvim: change dap bindings
    I forgot that <tab> is same as <C-I>, so yea...

--- dotfiles/.config/nvim/lua/dap_rc.lua
+++ dotfiles/.config/nvim/lua/dap_rc.lua
@@ -22,7 +22,7 @@ require("dapui").setup({
   },
   -- Expand lines larger than the window
   -- Requires >= 0.7
-  expand_lines = vim.fn.has("nvim-0.7"),
+  expand_lines = false,
   -- Layouts define sections of the screen to place windows.
   -- The position can be "left", "right", "top" or "bottom".
   -- The size specifies the height/width depending on position. It can be an Int
@@ -46,7 +46,7 @@ require("dapui").setup({
       elements = {
         "scopes",
       },
-      size = 0.5, -- 25% of total lines
+      size = 0.4, -- 25% of total lines
       position = "bottom",
     },
   },
--- dotfiles/.config/nvim/lua/keybindings.lua
+++ dotfiles/.config/nvim/lua/keybindings.lua
@@ -36,19 +36,19 @@ map("n", "<Space>k", ":Telescope keymaps<cr>")
 map("n", "<C-B>", ":SidebarNvimToggle<CR>")
 
 -- Debugging
-map("n", "<tab><tab>", ":lua require'dapui'.toggle()<CR>")
-map("n", "<tab>c", ":lua require'dap'.continue()<CR>")
-map("n", "<tab>s", ":lua require'dap'.close()<CR>")
-map("n", "<tab>ov" , ":lua require'dap'.step_over()<CR>")
-map("n", "<tab>in" , ":lua require'dap'.step_into()<CR>")
-map("n", "<tab>ou" , ":lua require'dap'.step_out()<CR>")
-map("n", "<tab>b" , ":lua require'dap'.toggle_breakpoint()<CR>")
-map("n", "<tab>tc" , ":lua require'dap'.run_to_cursor()<CR>")
-map("n", "<leader>t" , ":lua require'dap'.repl.toggle()<CR>")
+map("n", "<leader>b" , ":lua require'dap'.toggle_breakpoint()<CR>")
+map("n", "<leader><space>", ":lua require'dap'.continue()<CR>")
+map("n", "<F10>", ":lua require'dap'.close();require'dapui'.close()<CR>")
 map("n", "<leader>l" , ":lua require'dap'.run_last()<CR>")
-map("n", "<tab>h" , ":lua require'dap.ui.widgets'.hover()<CR>")
--- nnoremap <silent> <leader>B :lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>
+map("n", "<leader>c" , ":lua require'dap'.run_to_cursor()<CR>")
+
+map("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+map("n", "<F2>", ":lua require'dapui'.toggle()<CR>")
+map("n", "<leader>ov" , ":lua require'dap'.step_over()<CR>")
+map("n", "<leader>in" , ":lua require'dap'.step_into()<CR>")
+map("n", "<leader>ou" , ":lua require'dap'.step_out()<CR>")
+map("n", "<leader>t" , ":lua require'dap'.repl.toggle()<CR>")
 -- nnoremap <silent> <leader>lp :lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>
 
 -- Other
-map("n", "<leader>ec", ":echo synIDattr(synIDtrans(synID(line(\".\"), col(\".\"), 1)), \"name\")<CR>") --print color name under cursor
+-- map("n", "<leader>ec", ":echo synIDattr(synIDtrans(synID(line(\".\"), col(\".\"), 1)), \"name\")<CR>") --print color name under cursor
